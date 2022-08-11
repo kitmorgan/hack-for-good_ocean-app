ccdb3a9cac60eddbb3f9d4576289eb0eccb1d33d
Adam Aguilar <adamaguilar997@gmail.com>
2022-08-11 10:42:18 -0500

    added components

--- team-42/react-frontend/src/Components/Main.js
+++ team-42/react-frontend/src/Components/Main.js
@@ -20,17 +20,7 @@ const Main = () => (
       style={{
         padding: "0 50px",
       }}
-    >
-      <Breadcrumb
-        style={{
-          margin: "16px 0",
-        }}
-      >
-        <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>List</Breadcrumb.Item>
-        <Breadcrumb.Item>App</Breadcrumb.Item>
-      </Breadcrumb>
-    </Content>
+    ></Content>
     <h1
       style={{
         display: "flex",
