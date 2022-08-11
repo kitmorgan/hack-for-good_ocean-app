a1db8449259341e1057b30c537e9acdcd6e231f0
Jennifer Lee <tocontactjen@gmail.com>
2022-08-11 14:04:09 -0400

    Schema updates round 2

---
+++ team-42/Hack_for_good backend (new)/src/main/java/com/team42ETSE/Hack_for_good/dao/BeachDao.java
@@ -0,0 +1,11 @@
+package com.team42ETSE.Hack_for_good.dao;
+
+import com.team42ETSE.Hack_for_good.model.Beaches;
+
+import java.util.List;
+
+public interface BeachDao {
+
+    public List<Beaches> getBeaches();
+
+}
