20f5f5493e6dd06f92f14f53b65767b8bbc5cb95
Jennifer Lee <tocontactjen@gmail.com>
2022-08-11 13:26:05 -0400

    Database additions

--- team-42/Hack_for_good backend (new)/DBSchema/schema.sql
+++ team-42/Hack_for_good backend (new)/DBSchema/schema.sql
@@ -24,4 +24,30 @@ email varchar(250) NOT NULL,
 CONSTRAINT pk_event_user PRIMARY KEY (event_user_id),
 CONSTRAINT fk_event_id FOREIGN KEY (event_id) REFERENCES events (event_id));
 
+
+INSERT INTO beaches VALUES (null, 'Myrtle Beach', 'Florida');
+INSERT INTO beaches VALUES (null, 'Panama City Beach', 'Florida');
+INSERT INTO beaches VALUES (null, 'Cape May Beach', 'New Jersey');
+INSERT INTO beaches VALUES (null, 'Venice Beach', 'California');
+INSERT INTO beaches VALUES (null, 'Fort Lauderdale Beach', 'Florida');
+INSERT INTO beaches VALUES (null, 'Santa Monica Beach', 'California');
+INSERT INTO beaches VALUES (null, 'Ocean City Beach', 'Maryland');
+INSERT INTO beaches VALUES (null, 'Galveston Beach', 'Texas');
+INSERT INTO beaches VALUES (null, 'Gulf Shores Beach', 'Alabama');
+
+INSERT INTO events VALUES ('Galveston Beach','Help keep our Texas beaches clean!', '2022-08-22', 8);
+INSERT INTO events VALUES ('Santa Monica Beach','Join us this Saturday for a Santa Monica Beach clean-up', '2022-08-17', 6);
+INSERT INTO events VALUES ('Cape May Beach','Come down to the shore for our community clean-up day', '2022-08-15', 3);
+INSERT INTO events VALUES ('Panama City Beach','Beautifying the most beautiful beach in the world', '2022-08-24', 2);
+
+INSERT INTO event_users VALUES (1, 'hakeem@gmail.com');
+INSERT INTO event_users VALUES (1, 'kit@gmail.com');
+INSERT INTO event_users VALUES (1, 'erasmus@gmail.com');
+INSERT INTO event_users VALUES (1, 'kongyue@gmail.com');
+INSERT INTO event_users VALUES (1, 'adam@gmail.com');
+INSERT INTO event_users VALUES (1, 'serref@gmail.com');
+INSERT INTO event_users VALUES (1, 'jen@gmail.com');
+INSERT INTO event_users VALUES (1, 'pradeep@gmail.com');
+
+
 COMMIT;
