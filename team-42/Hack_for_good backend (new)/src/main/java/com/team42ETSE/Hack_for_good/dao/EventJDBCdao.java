97d34d9e364d8688a4eca5741e4a90480dd89758
kitm <kit.j.morgan@gmail.com>
2022-08-11 13:45:56 -0400

    event controller and dao

--- team-42/Hack_for_good backend (new)/src/main/java/com/team42ETSE/Hack_for_good/dao/EventJDBCdao.java
+++ team-42/Hack_for_good backend (new)/src/main/java/com/team42ETSE/Hack_for_good/dao/EventJDBCdao.java
@@ -1,4 +1,31 @@
 package com.team42ETSE.Hack_for_good.dao;
 
-public class EventJDBCdao {
+import com.team42ETSE.Hack_for_good.model.Event;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class EventJDBCdao implements EventDao{
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public EventJDBCdao(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    @Override
+    public List<Event> getEvents() {
+        String sql = "Select * From events;";
+        Event event = new Event();
+        SqlRowSet result = jdbcTemplate.queryForRowSet(sql);
+        while(result.next()){
+            event.setEvent_id(result.getInt("event_id"));
+            event.setBeach_id(result.getInt("beach_id"));
+            event.setDate(result.getString("date"));
+            event.setName(result.getString("name"));
+            event.setDescription(result.getString("description"));
+        }
+    }
 }
