10a337cb4a8578ca41a97a1c989b13d6c0b63607
Adam Aguilar <adamaguilar997@gmail.com>
2022-08-11 14:45:41 -0500

    added api call to Events

--- team-42/react-frontend/src/Components/Events.js
+++ team-42/react-frontend/src/Components/Events.js
@@ -4,30 +4,6 @@ import { Layout, Typography, Row, Col, Avatar, Card } from "antd";
 
 const { Text, Title } = Typography;
 
-export function EventCard() {
-  return (
-    <Card hoverable>
-      <div>
-        <Title level={4}>Beach</Title>
-        <img
-          width={200}
-          src={
-            "https://assets1.cbsnewsstatic.com/hub/i/r/2011/06/29/441f00d4-a643-11e2-a3f0-029118418759/thumbnail/620x465/3c495022ddcdaa44c93a4b4bf871958f/dirtybeach_istock_000015037586.jpg"
-          }
-          alt={"dirty beach"}
-        />
-      </div>
-      <p>brief description</p>
-      <div>
-        <div>
-          <Text>dirty </Text>
-        </div>
-        <Text>Miami</Text>
-      </div>{" "}
-    </Card>
-  );
-}
-
 class Events extends Component {
   constructor(props) {
     super(props);
@@ -44,16 +20,38 @@ class Events extends Component {
 
   render() {
     console.log(this.state.events);
+    const eventMap = this.state.events.map((event, i) => {
+      console.log(event);
+
+      return (
+        <Card hoverable>
+          <div>
+            <Title level={4}>{event.name}</Title>
+            <img
+              width={200}
+              src="https://assets1.cbsnewsstatic.com/hub/i/r/2011/06/29/441f00d4-a643-11e2-a3f0-029118418759/thumbnail/620x465/3c495022ddcdaa44c93a4b4bf871958f/dirtybeach_istock_000015037586.jpg"
+              alt={"dirty beach"}
+            />
+          </div>
+          <p>{event.description}</p>
+          <div>
+            <div>
+              <Text>{event.date} </Text>
+            </div>
+            <Text>{event.name}</Text>
+          </div>{" "}
+        </Card>
+      );
+
+      //   );
+    });
     return (
       <>
-        <h1> test test</h1>
+        <h1> Active Events</h1>
         <div>
-          {" "}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex temporibus
-          neque ipsa asperiores voluptatibus quis eveniet voluptatum, error vero
-          eos totam aliquam quibusdam, porro consequatur expedita delectus
-          reiciendis velit non.
-          <EventCard />
+          <Row gutter={[32, 32]} className="crypto-card-container">
+            <div>{eventMap}</div>
+          </Row>
         </div>
       </>
     );
