25378accf4996e227278831b8f98b362a93c6878
kitm <kit.j.morgan@gmail.com>
2022-08-11 13:27:20 -0400

    dao

---
+++ team-42/Hack_for_good backend (new)/src/main/java/com/team42ETSE/Hack_for_good/dao/EventDao.java
@@ -0,0 +1,11 @@
+package com.team42ETSE.Hack_for_good.dao;
+
+import com.team42ETSE.Hack_for_good.model.Event;
+
+import java.util.List;
+
+public interface EventDao {
+
+    public List<Event> getEvents();
+
+}
