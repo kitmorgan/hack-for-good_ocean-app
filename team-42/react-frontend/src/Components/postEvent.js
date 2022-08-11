4e97f48db05655789e95d4e7365b7780d171ce8b
hakeem <hakeemali34@gmail.com>
2022-08-11 13:38:51 -0500

    -postEvents form functionality completed

--- team-42/react-frontend/src/Components/postEvent.js
+++ team-42/react-frontend/src/Components/postEvent.js
@@ -1,5 +1,45 @@
 import React, { Component } from "react";
 import { withRouter, NavLink } from "react-router-dom";
+import 'antd/dist/antd.css';
+/*import './index.css';*/
+import { Button, DatePicker, Form, Input } from 'antd';
+
+export function PostEvent() {
+  return (
+      <Form name="nest-messages">
+        <Form.Item
+          name={['user', 'name']}
+          label="Beach Name"
+          rules={[
+            {
+              required: true
+            },
+          ]}>
+          <Input />
+        </Form.Item>
+    
+        <Form.Item 
+          label="DatePicker"
+          rules={[
+            {
+              required: true,
+            },
+          ]}>
+          <DatePicker />
+        </Form.Item>
+
+        <Form.Item name={['description']} label="Description">
+          <Input.TextArea />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+  );
+  }
 
 class postEvent extends Component {
   constructor(props) {
@@ -18,7 +58,9 @@ class postEvent extends Component {
           neque ipsa asperiores voluptatibus quis eveniet voluptatum, error vero
           eos totam aliquam quibusdam, porro consequatur expedita delectus
           reiciendis velit non.
+          <PostEvent />
         </div>
+        <></>
       </>
     );
   }
